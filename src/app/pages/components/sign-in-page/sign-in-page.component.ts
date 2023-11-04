d91844853376cc221a0174fa4e64e3ca622fb894
naka-mb <wat.surf.learning789@gmail.com>
2023-11-04 22:08:08 +0900

    sec29 サインイン画面作成と多言語化

--- src/app/pages/components/sign-in-page/sign-in-page.component.ts
+++ src/app/pages/components/sign-in-page/sign-in-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-sign-in-page',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public translateService: TranslateService) { }
 
   ngOnInit(): void {
+    // Sets language from browser settings.
+    this.setupLanguage();
   }
 
+// --------------------------------------------------------------------------------
+// private methods
+// --------------------------------------------------------------------------------
+  private setupLanguage() {
+    // Setups language using browser settings.
+    this.translateService.setDefaultLang(this.getLanguage(navigator.language));
+    this.translateService.use(this.getLanguage(navigator.language));
+  }
+
+  private getLanguage(language: string): string {
+    console.log('SignInPageComponent #getLanguage() language:' + language);
+
+    const CHAR_HYPHEN = '-';
+    if (language.indexOf(CHAR_HYPHEN) > 0) {
+      const splittedLanguage: string[] = language.split(CHAR_HYPHEN);
+      console.log('SignInPageComponent #getLanguage() splittedLanguage[0]:' + splittedLanguage[0]);
+
+      return splittedLanguage[0];
+    }
+    return language;
+  }
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,6 +5,8 @@ import { ProductListingPageComponent } from './components/product-listing-page/p
 import { ProductRegisteringPageComponent } from './components/product-registering-page/product-registering-page.component';
 import { StockRegisteringPageComponent } from './components/stock-registering-page/stock-registering-page.component';
 import { PurchaseHistoryListingPageComponent } from './components/purchase-history-listing-page/purchase-history-listing-page.component';
+import {NgxTranslateModule} from "../ngx-translate/ngx-translate.module";
+import {HttpClientModule} from "@angular/common/http";
 
 
 
@@ -17,7 +19,9 @@ import { PurchaseHistoryListingPageComponent } from './components/purchase-histo
     PurchaseHistoryListingPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    NgxTranslateModule,
   ],
   exports: [
     SignInPageComponent,
